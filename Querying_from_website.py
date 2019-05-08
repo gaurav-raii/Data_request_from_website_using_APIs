a3e34e61b7ec3a54744d7eac15348a5bf3003dfd
Gaurav Rai <gauravrai@tamu.edu>
2019-05-08 16:41:39 -0500

    Update Querying_from_website.py

--- Querying_from_website.py
+++ Querying_from_website.py
@@ -35,3 +35,20 @@ def pretty_print(data, indent=4):
     else:
         print(data)
 
+def main():
+    results = query_by_name(ARTIST_URL, query_type["simple"], "Nirvana")
+    pretty_print(results)
+
+    artist_id = results["artist"][1]["id"]
+    print ("\nARTIST:")
+    pretty_print(results["artist"][1])
+
+    artist_data = query_site(ARTIST_URL, query_type["releases"], artist_id)
+    releases = artist_data["releases"]
+    print ("\nONE RELEASE:")
+    pretty_print(releases[0], indent=2)
+    release_titles = [r["title"] for r in releases]
+
+    print ("\nALL TITLES:")
+    for t in release_titles:
+        print (t)
