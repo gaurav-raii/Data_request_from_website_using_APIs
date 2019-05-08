c96d7855a0ac2de4baf092a3c0f7732f6582c9d6
Gaurav Rai <gauravrai@tamu.edu>
2019-05-08 16:39:51 -0500

    Update Querying_from_website.py

--- Querying_from_website.py
+++ Querying_from_website.py
@@ -14,3 +14,13 @@ query_type = {  "simple": {},
                 "atr": {"inc": "aliases+tags+ratings"},
                 "aliases": {"inc": "aliases"},
                 "releases": {"inc": "releases"}}
+
+def query_site(url, params, uid="", fmt="json"):
+    params["fmt"] = fmt
+    r = requests.get(url + uid, params=params)
+    print ("requesting", r.url)
+
+    if r.status_code == requests.codes.ok:
+        return r.json()
+    else:
+        r.raise_for_status()
